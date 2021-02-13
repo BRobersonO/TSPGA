f23d7a62ab0860d31828542fbec381b9315c53ff
BranLWill <bwillett@knights.ucf.edu>
2021-02-13 17:32:33 -0500

    Added in a GetDistance method for the position class

--- Position.java
+++ Position.java
@@ -1,14 +1,19 @@
 //Object to hold the x and y position in the map
 public class Position 
 {
-        //Variables needed for the GA's
-        public float X;
-        public float Y;
-    
-        //Constructor for the Position object to assign the variables needed
-        public Position(float x_cord, float y_cord)
-        {
-            this.X = x_cord;
-            this.Y = y_cord;
-        }
+    //Variables needed for the GA's
+    public float X;
+    public float Y;
+
+    //Constructor for the Position object to assign the variables needed
+    public Position(float x_cord, float y_cord)
+    {
+        this.X = x_cord;
+        this.Y = y_cord;
+    }
+
+    public double GetDistance(Position otherPos)
+    {
+        return Math.sqrt((otherPos.X - this.X) * (otherPos.X - this.X) + (otherPos.Y - this.Y) * (otherPos.Y - this.Y));
+    }
 }
