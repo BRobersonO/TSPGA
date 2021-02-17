faefafe354bcad60f49c752b7962a74ebc712dba
BranLWill <bwillett@knights.ucf.edu>
2021-02-16 21:34:24 -0500

    Slight bug fix with the EOF check

--- CityMapData.java
+++ CityMapData.java
@@ -20,49 +20,50 @@ public class CityMapData
     {
         //Scanner to read in the file
         Scanner scanner = new Scanner(new File(filename));
-	//checks if the line just read in is the EOF line all the tsp files have.
-	if(line.equals("EOF")) {
-             break;
-        }
 
         //Loops through each line in the scanner until the end of the file
         while (scanner.hasNextLine()) 
         {
-           String line = scanner.nextLine();
+            String line = scanner.nextLine();
 
-           if(line.contains("NAME:"))
-           {
-               Name = line.substring(6);
-           }
-           else if (line.contains("TYPE:"))
-           {
-               FileType = line.substring(6);
-           }
-           else if (line.contains("COMMENT:"))
-           {
-               Comment = line.substring(9);
-           }
-           else if (line.contains("DIMENSION:")) 
-           {
-               Dimension = Integer.parseInt(line.substring(11));
-           }
-           else if (line.contains("EDGE_WEIGHT_TYPE:")) 
-           {
-               EDGE_WEIGHT_TYPE = line.substring(18);
-           }
-           else if (!line.contains("NODE_COORD_SECTION"))
-           {
-               //Splitting on whitespace characters
-               String[] cityVars = line.split("\\s");
+            //checks if the line just read in is the EOF line all the tsp files have.
+            if(line.equals("EOF")) {
+                break;
+            }
 
-               //Assigning the proper data
-               int cityID = Integer.parseInt(cityVars[0]);
-               float cityX = Float.parseFloat(cityVars[1]);
-               float cityY = Float.parseFloat(cityVars[2]);
+            if(line.contains("NAME:"))
+            {
+                Name = line.substring(6);
+            }
+            else if (line.contains("TYPE:"))
+            {
+                FileType = line.substring(6);
+            }
+            else if (line.contains("COMMENT:"))
+            {
+                Comment = line.substring(9);
+            }
+            else if (line.contains("DIMENSION:")) 
+            {
+                Dimension = Integer.parseInt(line.substring(11));
+            }
+            else if (line.contains("EDGE_WEIGHT_TYPE:")) 
+            {
+                EDGE_WEIGHT_TYPE = line.substring(18);
+            }
+            else if (!line.contains("NODE_COORD_SECTION"))
+            {
+                //Splitting on whitespace characters
+                String[] cityVars = line.split("\\s");
 
-               //Creating and adding in new city as they are read in from the file
-               citiesMap.put(cityID, new Position(cityX, cityY));
-           }
+                //Assigning the proper data
+                int cityID = Integer.parseInt(cityVars[0]);
+                float cityX = Float.parseFloat(cityVars[1]);
+                float cityY = Float.parseFloat(cityVars[2]);
+
+                //Creating and adding in new city as they are read in from the file
+                citiesMap.put(cityID, new Position(cityX, cityY));
+            }
         }
 	}
 
