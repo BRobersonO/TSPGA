7eced794e04e5cabd05ac3fc7cbe24bfaebe49cf
BranLWill <bwillett@knights.ucf.edu>
2021-02-20 12:16:59 -0500

    Added in the outline for the output

--- TSPMatrixSearch.java
+++ TSPMatrixSearch.java
@@ -88,35 +88,131 @@ public class TSPMatrixSearch extends FitnessFunction{
     
         //make a pop list
         List<TSPMatrixPathway> population = new ArrayList<>();
-        
-        int k;
-        for(k = 0; k < Parameters.popSize/*POP SIZE*/; k++) {
-            Random rand = new Random();
-            int x = rand.nextInt(matrix1.length); //our randomly selected start city
-            //initialize path to 0's
-            int j, jj;
-            int[][] path = new int[matrix1.length][matrix1.length];
-            for(j = 0; j < path.length; j++) {
-                for(jj = 0; jj < path.length; jj++) {
-                    path[j][jj] = 0;
-                }
-            }
 
-            double fitness = 0;
-            TSPMatrixPathway newWay = new TSPMatrixPathway(path, fitness);
-            
-            //clear the cities-visited list
-            visited.clear();  
-            
-            int error = step(matrix1, x, visited, population, newWay);
-            if(error == 1) {
-                System.out.println("step had an error");
-            }
+        //Looping through the number of runs
+        for (int r = 0; r < Parameters.numRuns; r++) 
+        {
+            InitialPopulation(matrix1, visited, population);
+
+            //Looping through the generations
+            //TODO: Get the best fit, Calculate the crossover and apply to the new generation
+            for (int g = 0; g < Parameters.generations; g++) 
+            {
+                //Get the best fit for the generation
+                //Get the best fit for the run
+                //Get the best fit for the overall
+
+                // Accumulate fitness statistics ****
+				fitnessStats[0][G] += sumRawFitness / Parameters.popSize;
+				fitnessStats[1][G] += bestOfGenChromoMap.rawFitness;
+				fitnessStats[2][G] += (sumRawFitness / Parameters.popSize) * (sumRawFitness / Parameters.popSize);
+				fitnessStats[3][G] += bestOfGenChromoMap.rawFitness * bestOfGenChromoMap.rawFitness;
+
+                averageRawFitness = sumRawFitness / Parameters.popSize;
+				stdevRawFitness = Math.sqrt(
+							Math.abs(sumRawFitness2 - 
+							sumRawFitness*sumRawFitness/Parameters.popSize)
+							/
+							(Parameters.popSize-1)
+							);
+
+				ninetyFiveLow = averageRawFitness - (stdevRawFitness / Math.sqrt(Parameters.popSize)) * 2;
+				ninetyFiveHigh = averageRawFitness + (stdevRawFitness / Math.sqrt(Parameters.popSize)) * 2;
+
+				// Output generation statistics to screen
+				System.out.println(R + "\t" + G +  "\t" + (int)bestOfGenChromoMap.rawFitness + "\t" + averageRawFitness + "\t" + stdevRawFitness);
+			
+				// Output generation statistics to summary file
+				summaryOutput.write(" R ");
+				Hwrite.right(R, 3, summaryOutput);
+				summaryOutput.write("\t\t G ");
+				Hwrite.right(G, 3, summaryOutput);
+				summaryOutput.write("\t\t BestInG ");
+				Hwrite.right((int)bestOfGenChromoMap.rawFitness, 7, summaryOutput);
+				summaryOutput.write("\t\t AvgOfG ");
+				Hwrite.right(averageRawFitness, 11, 3, summaryOutput);
+				summaryOutput.write("\t\t StDv ");
+				Hwrite.right(stdevRawFitness, 11, 3, summaryOutput);
+				summaryOutput.write("\t\t 95% ConfInt ");
+				Hwrite.right(ninetyFiveLow, 11, 3, summaryOutput);
+				Hwrite.right(ninetyFiveHigh, 11, 3, summaryOutput);
+				summaryOutput.write("\n");
+
+			// *********************************************************************
+			// **************** SCALE FITNESS OF EACH MEMBER AND SUM ***************
+			// *********************************************************************
+
+                // No change to raw fitness
+                for (int i=0; i<Parameters.popSize; i++)
+                {
+                    memberMap[i].sclFitness = memberMap[i].rawFitness + .000001;
+                    sumSclFitness += memberMap[i].sclFitness;
+
+                    // PROPORTIONALIZE SCALED FITNESS FOR EACH MEMBER AND SUM
+                    memberMap[i].proFitness = memberMap[i].sclFitness/sumSclFitness;
+					sumProFitness = sumProFitness + memberMap[i].proFitness;
+                }
+
+            // // *********************************************************************
+			// // ************ CROSSOVER AND CREATE NEXT GENERATION *******************
+			// // *********************************************************************
+
+				// int parent1 = -1;
+				// int parent2 = -1;
+
+				// //  Assumes always two offspring per mating
+				// for (int i=0; i<Parameters.popSize; i=i+2)
+				// {
+				// 	//	Select Two Parents
+				// 	parent1 = ChromoMap.selectParent();
+				// 	parent2 = parent1;
+
+				// 	while (parent2 == parent1)
+				// 	{
+				// 		parent2 = ChromoMap.selectParent();
+				// 	}
+
+				// 	//	Crossover Two Parents to Create Two Children
+				// 	randnum = r.nextDouble();
+				// 	if (randnum < Parameters.xoverRate)
+				// 	{
+				// 		ChromoMap.mateParents(memberMap[parent1], memberMap[parent2], childMap[i], childMap[i+1]);
+				// 	}
+				// 	else 
+				// 	{
+				// 		ChromoMap.mateParents(memberMap[parent1], childMap[i]);
+				// 		ChromoMap.mateParents(memberMap[parent2], childMap[i+1]);
+				// 	}
+				// } // End Crossover
+
+                // //	Mutate Children
+				// for (int i=0; i<Parameters.popSize; i++)
+				// {
+				// 	//System.out.println(childMap[i].chromo.size());
+				// 	childMap[i].doMutation();
+				// }
+
+				// //	Swap Children with Last Generation
+				// for (int i=0; i<Parameters.popSize; i++)
+				// {
+				// 	ChromoMap.copyB2A(memberMap[i], childMap[i]);
+				// }
+            }//  Repeat the above loop for each generation
+
+            Hwrite.left(bestOfRunR, 4, summaryOutput);
+			Hwrite.right(bestOfRunG, 4, summaryOutput);
+
+			bestEaRunSum += (int)bestOfRunChromoMap.rawFitness;
+			bestEaRunSum2 += (int)bestOfRunChromoMap.rawFitness * (int)bestOfRunChromoMap.rawFitness;
+
+			problem.doPrintGenes(bestOfRunChromoMap, summaryOutput);
+
+			System.out.println(R + "\t" + "B" + "\t"+ (int)bestOfRunChromoMap.rawFitness);
         }
-        
+
         //We now have a population
-        //we need to do crossover, then select who's in, who's out.
         //we need to measure fitness, produce report on this generation
+        //we need to do crossover, then select who's in, who's out.
         //repeat -> produce a new generation?
 
         int h;
@@ -207,5 +303,32 @@ public class TSPMatrixSearch extends FitnessFunction{
         Random rand = new Random();
         int chance = rand.nextInt(100);
         return chance < (mutRate * 100); //represents a mutation rate of mutRate
+    }
+
+    private void InitialPopulation(double[][] matrix1, List<Integer> visited, List<TSPMatrixPathway> population)
+    {
+        for(int k = 0; k < Parameters.popSize/*POP SIZE*/; k++) {
+            Random rand = new Random();
+            int x = rand.nextInt(matrix1.length); //our randomly selected start city
+            //initialize path to 0's
+            int j, jj;
+            int[][] path = new int[matrix1.length][matrix1.length];
+            for(j = 0; j < path.length; j++) {
+                for(jj = 0; jj < path.length; jj++) {
+                    path[j][jj] = 0;
+                }
+            }
+
+            double fitness = 0;
+            TSPMatrixPathway newWay = new TSPMatrixPathway(path, fitness);
+            
+            //clear the cities-visited list
+            visited.clear();  
+            
+            int error = step(matrix1, x, visited, population, newWay);
+            if(error == 1) {
+                System.out.println("step had an error");
+            }
+        } // Population creation
     }
 }
